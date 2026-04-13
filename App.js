060cdf1393c5d17ed684a96ef215f8adb045ee40
beastswise-ui <beastswise@gmail.com>
2026-04-13 16:31:07 +0200

    Create App.js

---
+++ App.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export default function App() {
+  return (
+    <div>
+      <h1>PURE IS ONLINE</h1>
+      <p>Als je dit ziet, werkt de verbinding!</p>
+    </div>
+  );
+}
